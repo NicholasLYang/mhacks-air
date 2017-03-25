cae05c712e58de4861f177b11dfa5221c1e0d8d2
Nicholas Yang <nick@nicholasyang.com>
2017-03-25 17:33:33 -0400

    airplane models

--- app/javascript/packs/vr/components/Wing.js
+++ app/javascript/packs/vr/components/Wing.js
@@ -11,15 +11,49 @@ class Wing extends BaseComponent {
         this.render = this.render.bind(this);
     }
 
-    render() { 
+    render() {
         return (
+            <Entity>
             <Entity
-            position={this.translate(this.props,[0,5,3])}
+            position={this.translate(this.props, [1, 4.6, 3])}
             rotation="90 0 0"
             scale="1 10 1"
             id="plane-wings"
             geometry="mergeTo:null;depth:0.1"
             material="side:double;color:#0d4cff;normalScale:{&quot;x&quot;:null}"/>
+            <Entity
+            position={this.translate(this.props, [1, 5.4, 3])}
+            rotation="90 0 0"
+            scale="1 10 1"
+            id="plane-wings"
+            geometry="mergeTo:null;depth:0.1"
+            material="side:double;color:#0d4cff;normalScale:{&quot;x&quot;:null}"/>
+
+                <Entity>
+                <Entity primitive="a-cylinder"
+            position={this.translate(this.props, [1.35, 5, 7.75])}
+            geometry="radius: 0.05" />
+
+                <Entity primitive="a-cylinder"
+            position={this.translate(this.props, [1.35, 5, -1.75])}
+            geometry="radius: 0.05" />
+                <Entity primitive="a-cylinder"
+            position={this.translate(this.props, [0.65, 5, 7.75])}
+            geometry="radius: 0.05" />
+
+                <Entity primitive="a-cylinder"
+            position={this.translate(this.props, [0.65, 5, -1.75])}
+            geometry="radius: 0.05" />
+                </Entity>
+
+                <Entity primitive="a-box"
+            position={this.translate(this.props, [-2.5, 5.35, 3])}
+            rotation="0 0 45"
+            scale="1.5 1.5 0.1"
+                />
+
+                </Entity>
+
         );
     };
 
